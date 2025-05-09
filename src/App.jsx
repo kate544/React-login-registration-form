fd04aac389b496b78864c741ee9a8f02d571ae2b
kathryntokoli@gmail.com <>
2025-05-09 15:48:11 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginForm from './Components/LoginRegister/LoginRegister'
+
+const App = () => {
+  return (
+    <div>
+      <LoginForm/>
+    </div>
+  )
+}
+
+export default App
